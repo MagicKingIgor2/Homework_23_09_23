8b637cd1b3b25d5de66520b7dd8a5fd1c80a4c8f
Igor <gosha99_2010@inbox.ru>
2023-10-01 18:09:51 +0300

    Добавил зачу №13 из ДЗ к семинару 2

---
+++ Task06/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int Figure(string message) {
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int thirdrank(int number) {
+    while (number > 999) {
+        number /= 10;
+    }
+    return number % 10;
+}
+bool neednumber(int number) {
+    if (number < 100) {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = Figure("Введите число >");
+if (neednumber(number)) {
+    Console.WriteLine(thirdrank(number));
+}
