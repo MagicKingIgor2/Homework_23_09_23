492a7bf4c2216f7a70d9861f9d8fbe1e2f98f1ed
Igor <gosha99_2010@inbox.ru>
2023-10-07 10:38:06 +0300

    Добавил задачу 34, которая ищет четные элементы массива, состоящего из трехзначных чисел

---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+void array(int a) {
+    for (int i = 0; i < a; i++) {
+        randomArray[i] = new Random().Next(99,999+1);
+        Console.WriteLine(randomArray[i] + " ");
+    }
+}
+int Length(int[] randomArray) {
+    int Length = 0;
+    for (int i = 0; i < randomArray.Length; i++) {
+        if (randomArray[i] % 2 == 0)
+        Length ++;
+    }
+    return Length;
+}
+array(a);
+Console.Write($"Количество чётных чисел в массиве: {Length(randomArray)}");
