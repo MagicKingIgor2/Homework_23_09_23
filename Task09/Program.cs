80997aafb717ac7c37494d96fdf18c24c7483ca9
Igor <gosha99_2010@inbox.ru>
2023-10-07 09:06:24 +0300

    Написал программу, выполняющую требования к задаче

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,20 @@
-﻿message = "привет";
-Console.Write(message);
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+int Prompt(string message) {
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int sum(int number) {
+    int result = 0;
+    while (number > 0) {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}   
+int number = Prompt("Введите число: ");
+Console.WriteLine($"Сумма всех цифр в числе {number} равна {sum(number)}");
